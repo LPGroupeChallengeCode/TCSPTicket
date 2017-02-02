b18f32e782a029dec05bf522fb7cece24a3c0f99
xCoolT <ludovic.robertin@gmail.com>
2017-02-02 16:03:52 -0400

    update_ludovic#1

--- API/routes/tickets.js
+++ API/routes/tickets.js
@@ -5,3 +5,22 @@ module.exports = function(app) {
 	        res.end();
 	});
 }
+
+//COMMANDE TICKET *** EN COURS *****
+app.put('/ticket/:id/user', auth, function(req, res, next){
+	Ticket.findById(req.params.id, function(err, liste){
+		if(err){console.log(err);
+			return next(err);}
+	});
+});
+
+
+//LISTE TICKET
+	app.get('/liste/:id', function(req, res, next){
+		//recup info
+		Liste.findById({'user.ticket.id': req.params.id}, function(err, listes){
+			if(err){console.log(err); return next(err);}
+		//Envoie rep
+		res.json(listes);
+		});
+	});
--- API/routes/users.js
+++ API/routes/users.js
@@ -5,3 +5,31 @@ module.exports = function(app) {
 	        res.end();
 	});
 }
+
+//LOGIN
+	app.post('/login', function(req, res, next){
+		if(!req.body.mail || !req.body.password){
+			return res.status(400).json({message: 'Veuillez remplir tous les champs'});
+		}
+
+	db.User.findOne(
+		{mail: req.body.mail},
+		{password: req.body.password})
+
+	if(user){
+				return res.json({token: user.generateJWT()});
+			}else{
+				return res.status(401).json(info);
+			}
+		(req, res, next);
+	});
+
+//REGISTER
+	app.post('/register', function(req, res, next){
+		//INSERT DB
+		db.User.insert(
+		{name : req.body.name},
+		{firstname : req.body.firstname},
+		{mail : req.body.mail},
+		{password : req.body.password})
+		})
