8d040c5ef9284ee243ad4ea61f5848e5f9814a92
alix96 <Oriane@MacBook-Pro-de-Oriane.local>
2017-02-09 11:47:36 -0400

    update android #1

---
+++ Android/BilletterieTCSP/app/src/main/java/com/dev/tropic404/billetterietcsp/LoginActivity.java
@@ -0,0 +1,14 @@
+package com.dev.tropic404.billetterietcsp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
